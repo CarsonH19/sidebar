4b96a64ad026b5f59ffd7f208369da58d536e50d
CarsonH19 <questline.carson@gmail.com>
2023-10-14 23:37:10 +0900

    fix: footer fade doesn't block icons

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const openBars = document.querySelector('.sidebar-toggle');
-const aside = document.querySelector('.sidebar');
-const closeButton = document.querySelector('.close-btn');
-const footer = document.querySelector('footer');
+const openBars = document.querySelector(".sidebar-toggle");
+const aside = document.querySelector(".sidebar");
+const closeButton = document.querySelector(".close-btn");
+const footer = document.querySelector("footer");
 
 function openSidebar() {
-  aside.style.transform = 'translate(0%)';
-  footer.classList.add('fadeOut');
-};
+  aside.style.transform = "translate(0%)";
+  footer.classList.remove('fadeIn');
+  footer.classList.add("fadeOut");
+  setTimeout(() => {
+    footer.classList.add("displayNone");
+  }, 500);
+}
 
 function closeSidebar() {
-  aside.style.transform = 'translate(-100%)';
-  footer.classList.remove('fadeOut');
-};
+  aside.style.transform = "translate(-100%)";
+  footer.classList.remove("fadeOut");
+  footer.classList.remove("displayNone");
+  footer.classList.add("transparent");
+  setTimeout(() => {
+    footer.classList.remove("transparent");
+    footer.classList.add("fadeIn");
+  }, 400);
+}
 
-
-openBars.addEventListener('click', openSidebar);
-closeButton.addEventListener('click', closeSidebar);
-
+openBars.addEventListener("click", openSidebar);
+closeButton.addEventListener("click", closeSidebar);
