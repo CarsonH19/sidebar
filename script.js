4d6ed040f4dd01d0ba02804bbed61e1bbeab8070
CarsonH19 <questline.carson@gmail.com>
2023-10-14 23:00:37 +0900

    feat: footer fades in and out
    When the bars are clicked the footer fades out. When the 'X' is clicked the footer fades in.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const openBars = document.querySelector('.sidebar-toggle');
 const aside = document.querySelector('.sidebar');
 const closeButton = document.querySelector('.close-btn');
+const footer = document.querySelector('footer');
 
 function openSidebar() {
   aside.style.transform = 'translate(0%)';
+  footer.classList.add('fadeOut');
 };
 
 function closeSidebar() {
   aside.style.transform = 'translate(-100%)';
+  footer.classList.remove('fadeOut');
 };
 
 
